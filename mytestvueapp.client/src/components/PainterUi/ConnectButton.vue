dd0e5490cbbd1d596f498f1a80648e937676359b
JacobSletten <slettenj2628@my.uwstout.edu>
2025-04-30 21:26:15 -0500

    Removed redundant button tag

--- mytestvueapp.client/src/components/PainterUi/ConnectButton.vue
+++ mytestvueapp.client/src/components/PainterUi/ConnectButton.vue
@@ -28,7 +28,7 @@
                             <Column field="memberCount" header="Count" class="w-2 p-1 h-1"></Column>
                             <column class="w-1 p-1 h-1">
                                 <template #body="{data}">
-                                    <Button label="Join" class="p-1 m-0" @click="groupname = data.groupName; connect(false)"></Button>
+                                    <Button label="Join" class="p-1 m-0" @click="groupname = data.groupName; connect(false)"/>
                                 </template>
                             </column>
                         </DataTable>
